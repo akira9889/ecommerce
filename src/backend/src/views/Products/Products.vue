81961dd239b371c1b339dc6c75fd81695b9d6ae6
akira9889 <aki_badmin89@icloud.com>
2023-05-25 09:41:25 +0900

    Refactor: 商品テーブルのコードをProductsTableコンポーネントに分割し、ディレクトリ構造を再編成

---
+++ src/backend/src/views/Products/Products.vue
@@ -0,0 +1,18 @@
+<script setup>
+import ProductsTable from './ProductsTable.vue'
+</script>
+
+<template>
+    <div class="flex items-center justify-between mb-3">
+        <h1 class="text-3xl font-semibold">商品一覧</h1>
+        <button type="submit"
+            class="py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            商品を追加
+        </button>
+    </div>
+    <ProductsTable />
+</template>
+
+
+<style scoped>
+</style>
