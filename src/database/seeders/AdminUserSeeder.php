4bcbe3f5b1774d26589b4719296bc2189df9ad03
akira9889 <aki_badmin89@icloud.com>
2023-06-17 13:13:42 +0900

    :sparkles: 挿入データーの追加

--- src/database/seeders/AdminUserSeeder.php
+++ src/database/seeders/AdminUserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,12 +16,29 @@ class AdminUserSeeder extends Seeder
      */
     public function run()
     {
-        User::create([
-            'name' => 'Admin',
-            'email' =>'admin@example.com',
-            'email_verified_at' => now(),
-            'password' => bcrypt('admin123'),
-            'is_admin' => true
-        ]);
+        $users = [
+            [
+                'name' => 'Admin',
+                'email' => 'admin@example.com',
+                'email_verified_at' => now(),
+                'password' => bcrypt('admin123'),
+                'is_admin' => true
+            ],
+            [
+                'name' => '岩澤 明',
+                'email' => 'aki_badmin89@icloud.com',
+                'email_verified_at' => now(),
+                'password' => bcrypt('password'),
+            ]
+        ];
+        foreach ($users as $user) {
+            $user = User::create($user);
+            $customer = new Customer();
+            $names = explode(" ", $user->name);
+            $customer->user_id = $user->id;
+            $customer->last_name = $names[0];
+            $customer->first_name = $names[1] ?? '';
+            $customer->save();
+        }
     }
 }
