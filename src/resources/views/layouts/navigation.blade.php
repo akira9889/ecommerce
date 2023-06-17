39218327e266633ee022664910c38d43a10b53ee
akira9889 <aki_badmin89@icloud.com>
2023-06-17 13:17:00 +0900

    :+1: ｀/profile｀への名前付きルートに変更

--- src/resources/views/layouts/navigation.blade.php
+++ src/resources/views/layouts/navigation.blade.php
@@ -1,9 +1,8 @@
 <header x-data="{
     mobileMenuOpen: false,
     cartItemsCount: {{ \App\Http\Helpers\Cart::getCartItemsCount() }}
-}"
-@cart-change.window="cartItemsCount = $event.detail.count"
-class="flex justify-between bg-slate-800 shadow-md text-white">
+}" @cart-change.window="cartItemsCount = $event.detail.count"
+    class="flex justify-between bg-slate-800 shadow-md text-white">
     <div>
         <a href="{{ route('home') }}" class="block py-navbar-item pl-5"> Logo </a>
     </div>
@@ -18,7 +17,8 @@ class="flex justify-between bg-slate-800 shadow-md text-white">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2 -mt-1" fill="none"
                             viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                             <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                                d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0
+                                 014 0z" />
                         </svg>
                         カート
                     </div>
@@ -48,7 +48,7 @@ class="flex justify-between bg-slate-800 shadow-md text-white">
                     </a>
                     <ul x-show="open" x-transition class="z-10 right-0 bg-slate-800 py-2">
                         <li>
-                            <a href="/src/profile.html" class="flex px-3 py-2 hover:bg-slate-900">
+                            <a href="{{ route('profile') }}" class="flex px-3 py-2 hover:bg-slate-900">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none"
                                     viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                     <path stroke-linecap="round" stroke-linejoin="round"
@@ -154,7 +154,7 @@ class="flex justify-between bg-slate-800 shadow-md text-white">
                     <ul @click.outside="open = false" x-show="open" x-transition x-cloak
                         class="absolute z-10 right-0 bg-slate-800 py-2 w-48">
                         <li>
-                            <a href="/src/profile.html" class="flex px-3 py-2 hover:bg-slate-900">
+                            <a href="{{ route('profile') }}" class="flex px-3 py-2 hover:bg-slate-900">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" fill="none"
                                     viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                     <path stroke-linecap="round" stroke-linejoin="round"
