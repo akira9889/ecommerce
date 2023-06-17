394e13076f65e22757264efc4c33de6fbd614da6
akira9889 <aki_badmin89@icloud.com>
2023-06-17 13:24:40 +0900

    :sparkles: `x-text-input`コンポーネントの`select`タイプの追加

--- src/resources/views/components/text-input.blade.php
+++ src/resources/views/components/text-input.blade.php
@@ -1,10 +1,22 @@
-@props(['disabled' => false, 'errors'])
+@props(['disabled' => false, 'errors', 'type' => 'text'])
 
 <?php
   $errorClasses = 'border-red-600 focus:border-red-600 ring-1 ring-red-600 focus:ring-red-600';
   $defaulClasses = '';
-  $successClasses = 'border-emerald-500 focus:border-emerald-500 ring-1 ring-emerald-500 focus:ring-emerald-500'
+  $successClasses = 'border-emerald-500 focus:border-emerald-500 ring-1 ring-emerald-500 focus:ring-emerald-500';
+
+  $attributeName = preg_replace('/(\w+)\[(\w+)]/', '$1.$2', $attributes['name']);
 ?>
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-2 border-gray-300 focus:border-purple-500 focus:outline-none focus:ring-purple-500 rounded-md w-full py-3 px-2 ' .
-($errors->has($attributes['name']) ? $errorClasses : (old($attributes['name']) ? $successClasses : $defaulClasses))
-]) !!}>
+<div>
+  @if ($type === 'select')
+    <select {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-2 border-gray-300 focus:border-purple-500 focus:outline-none focus:ring-purple-500 rounded-md w-full py-3 px-2 ' .
+    ($errors->has($attributeName) ? $errorClasses : (old($attributeName) ? $successClasses : $defaulClasses))
+    ]) !!}>
+      {{ $slot }}
+    </select>
+  @else
+    <input {{ $disabled ? 'disabled' : '' }} type="{{ $type }}" {!! $attributes->merge(['class' => 'border-2 border-gray-300 focus:border-purple-500 focus:outline-none focus:ring-purple-500 rounded-md w-full py-3 px-2 ' .
+    ($errors->has($attributeName) ? $errorClasses : (old($attributeName) ? $successClasses : $defaulClasses))
+    ]) !!}>
+  @endif
+</div>
