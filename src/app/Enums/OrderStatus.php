f33a755b0e6b6a37f4f2e57b4e157e4d3578efb1
akira9889 <aki_badmin89@icloud.com>
2023-06-27 12:46:19 +0900

    :+1: 新しいステータスとそれに対応する値を追加

--- src/app/Enums/OrderStatus.php
+++ src/app/Enums/OrderStatus.php
@@ -6,5 +6,18 @@ enum OrderStatus: string
 {
     case Unpaid = 'unpaid';
     case Paid = 'paid';
+    case Canceled = 'canceled';
+    case Shipped = 'shipped';
     case Completed = 'completed';
+
+    public static function getStatuses()
+    {
+        return [
+            self::Unpaid->value => '未払い',
+            self:: Paid->value => '支払い済み',
+            self:: Canceled->value => 'キャンセル',
+            self:: Shipped->value => '出荷済み',
+            self:: Completed->value => '完了',
+        ];
+    }
 }
