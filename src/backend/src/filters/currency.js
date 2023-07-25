4de60aa3a697921c6db70736ab88a046cba7edce
akira9889 <aki_badmin89@icloud.com>
2023-07-25 23:29:04 +0900

    :+1: オプションの削除

--- src/backend/src/filters/currency.js
+++ src/backend/src/filters/currency.js
@@ -1,3 +1,3 @@
 export default function currencyJPY(value) {
-  return new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY', maximumSignificantDigits: 3 }).format(value);
+  return new Intl.NumberFormat('ja-JP', { style: 'currency', currency: 'JPY' }).format(value);
 }
